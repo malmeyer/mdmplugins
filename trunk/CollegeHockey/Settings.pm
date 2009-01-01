cda1d5a808975a91dec5ce0a487e0589829cf461
malmeyer@hotmail.com <malmeyer@hotmail.com@23c9037a-d77f-11dd-8772-d1b041bf0e37>
2009-01-01 04:05:36 +0000

    initial

---
+++ trunk/CollegeHockey/Settings.pm
@@ -0,0 +1,30 @@
+package Slim::Plugin::CollegeHockey::Settings;
+
+# SqueezeCenter Copyright 2001-2007 Logitech.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+
+use strict;
+use base qw(Slim::Web::Settings);
+
+use Slim::Utils::Prefs;
+
+my $prefs = preferences('plugin.collegehockey');
+
+sub name {
+	return Slim::Web::HTTP::protectName('PLUGIN_SCREENSAVER_COLLEGEHOCKEY');
+}
+
+sub page {
+	return Slim::Web::HTTP::protectURI('plugins/CollegeHockey/settings/basic.html');
+}
+
+sub prefs {
+	return ($prefs, qw(pref_team1 pref_team2));
+}
+
+
+1;
+
+__END__
